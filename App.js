d6180220ed0f31aa875c6ae25113274c35949040
Daniel Dager <dager.verde@gmail.com>
2020-08-20 22:49:13 -0400

    first hooks

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [outputText, setOutputText] = useState("Start Tracking");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button
+        title="Start Tracking"
+        onPress={() => setOutputText("Now Tracking")}
+      />
+      <Button title="Click Me!" onPress={() => setCount(count + 1)} />
+      <Text>You have pressed {count} times.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
